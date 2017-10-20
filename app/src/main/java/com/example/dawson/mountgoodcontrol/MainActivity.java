8f9cd0417fa13b0bcf30a0fc8131cd4ec1eaf2e0
dawsondeere <dawsondeere@tamu.edu>
2017-10-20 15:21:21 -0500

    removed commented out code for sending battery level to Pebble watch

--- app/src/main/java/com/example/dawson/mountgoodcontrol/MainActivity.java
+++ app/src/main/java/com/example/dawson/mountgoodcontrol/MainActivity.java
@@ -13,18 +13,12 @@ import com.android.volley.Response;
 import com.android.volley.VolleyError;
 import com.android.volley.toolbox.StringRequest;
 import com.android.volley.toolbox.Volley;
-//import com.getpebble.android.kit.PebbleKit;
-//import com.getpebble.android.kit.util.PebbleDictionary;
-
-//import java.util.UUID;
 
 public class MainActivity extends AppCompatActivity {
     public static RequestQueue queue;
     public static String IP = "";
     public static final float SWIPE_THRESHOLD_VELOCITY = 200;
     public static final DefaultRetryPolicy retryPolicy = new DefaultRetryPolicy(DefaultRetryPolicy.DEFAULT_TIMEOUT_MS, 0, DefaultRetryPolicy.DEFAULT_BACKOFF_MULT);
-    //private static PebbleThread pebbleThread;
-    //private static final UUID pebUUID = UUID.fromString("1786e94e-ad90-496f-819a-0c9db99c174b");
 
     public static void writeData(String url) {
         String finalurl = "http://" + IP + ":5000/" + url;
@@ -69,49 +63,11 @@ public class MainActivity extends AppCompatActivity {
         stringRequest.setRetryPolicy(retryPolicy);
         queue.add(stringRequest);
     }
-/*
-    private class PebbleThread extends Thread {
-        private boolean cont;
 
-        private PebbleThread() { cont = true; }
-        private void cancel() { cont = false; }
-
-        public void run() {
-            Intent batteryStatus = getApplicationContext().registerReceiver(null, new IntentFilter(Intent.ACTION_BATTERY_CHANGED));
-            if (batteryStatus != null) {
-                while (cont) {
-                    try {
-                        PebbleDictionary dict = new PebbleDictionary();
-                        final int AppKeyBatteryLevel = 0;
-                        final int batteryLevel = batteryStatus.getIntExtra(BatteryManager.EXTRA_LEVEL, -1);
-                        dict.addInt32(AppKeyBatteryLevel, batteryLevel);
-                        PebbleKit.sendDataToPebble(getApplicationContext(), pebUUID, dict);
-                        Thread.sleep(1000);
-                    } catch (InterruptedException e) {
-                        System.out.println("Thread interrupted");
-                    }
-                }
-            }
-        }
-    }
-*/
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        /*
-        if (PebbleKit.isWatchConnected(getApplicationContext())) {
-            pebbleThread = new PebbleThread();
-            pebbleThread.start();
-        }
-        */
         queue = Volley.newRequestQueue(this);
         startActivity(new Intent(this, LightsActivity.class));
     }
-/*
-    @Override
-    protected void onDestroy() {
-        if (pebbleThread != null) { pebbleThread.cancel(); }
-        super.onDestroy();
-    }
-    */
 }
